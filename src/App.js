7852087bc70f731228d1655ea25b5f09802b0ebc
Tchouanfe feze boris <borisfeze9@gmail.com>
2023-08-30 10:25:45 +0100

    route bug fixed

--- src/App.js
+++ src/App.js
@@ -1,36 +1,36 @@
-import './App.css'
-import { BrowserRouter,Switch,Route} from 'react-router-dom';
-import Navbar  from './components/Navbar';
-import Home from './pages/home/Home';
-import Search from './pages/search/Search';
-import Recipe from './pages/recipe/Recipe';
-import Create from './pages/create/Create';
-import { Link } from 'react-router-dom';
+import "./App.css";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/home/Home";
+import Search from "./pages/search/Search";
+import Recipe from "./pages/recipe/Recipe";
+import Create from "./pages/create/Create";
+import { Link } from "react-router-dom";
 function App() {
   return (
     <div className="App">
       <h1>This App</h1>
       <BrowserRouter>
         <Link>
-         <Navbar />
+          <Navbar />
         </Link>
-          <Switch>
-             <Route exact path="/">
-                <Home/>
-              </Route>
-              <Route path="/search">
-                <Search/>
-              </Route>
-              <Route path="/create">
-                <Create/>
-              </Route>
-              <Route path="/recipe/:id" element={ <Recipe/>}/>
-               
-             
-         </Switch> 
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/search">
+            <Search />
+          </Route>
+          <Route path="/create">
+            <Create />
+          </Route>
+          <Route path="/recipe/:id">
+            <Recipe />
+          </Route>
+        </Switch>
       </BrowserRouter>
     </div>
   );
 }
 
-export default App
+export default App;
--- src/pages/recipe/Recipe.js
+++ src/pages/recipe/Recipe.js
@@ -1,34 +1,28 @@
-import {useParams} from 'react-router-dom'
-import { useFetch } from '../../hooks/useFetch';
+import { useParams } from "react-router-dom";
+import { useFetch } from "../../hooks/useFetch";
 function Recipe() {
-     const{id}=useParams();
-     const url = `http://localhost:3000/recipes/${id}`;;
-    const {data,isPending,error}=useFetch(url);
-   
-    return (
-        <div className='recipe'>
-            {error  && <div className='error'>{error}</div>}
-            {isPending  && <div className='loading'>loading...</div>}
-              
-              { data &&   (  
-                       <>  
-                       <ul>    
-                         
-                       </ul>          
-                         <h2  className='page-title'>{data.title}</h2>
-                         {data.ingredients.map(element => 
-                        (
-                            <li>{element}</li>
-                        )
-                       )}   
-                         <p>{data.cookingTime }</p>
-                          <div className='method'>  {data.method}</div>
-                         </>  
-                        ) }
-              
-            
-     </div>
-    )
+  const { id } = useParams();
+  const url = `http://localhost:3000/recipes/${id}`;
+  const { data, isPending, error } = useFetch(url);
+
+  return (
+    <div className="recipe">
+      {error && <div className="error">{error}</div>}
+      {isPending && <div className="loading">loading...</div>}
+
+      {data && (
+        <>
+          <ul></ul>
+          <h2 className="page-title">{data.title}</h2>
+          {data.ingredients.map((element) => (
+            <li>{element}</li>
+          ))}
+          <p>{data.cookingTime}</p>
+          <div className="method"> {data.method}</div>
+        </>
+      )}
+    </div>
+  );
 }
 
-export default Recipe
+export default Recipe;
